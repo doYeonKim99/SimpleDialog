a80ec57a9fe26e98d38b250b2f493bd83fa8998d
Doyeon Kim <ehdus859599@gmail.com>
2016-06-07 20:45:49 +0900

    #4 Change Textbox

--- app/src/main/java/kr/hs/emirim/dok96/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dok96/simpledialog/MainActivity.java
@@ -11,26 +11,33 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 
 {
+    String[] items=new String[]{"젤리빈","킷켓","롤리팝"};
+    Button buy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buy=(Button)findViewById(R.id.but_dialog);
+        buy=(Button)findViewById(R.id.but_dialog);
         buy.setOnClickListener(this);
     }
 
 
     public void onClick(View v){
+
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("첫 대화 상자");
-        dialog.setMessage("This is message part.");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                buy.setText(items[which]);
+            }
+        });
         dialog.setIcon(R.drawable.firsticon);
         dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(MainActivity.this,"대화 상자의 확인 버튼을 클릭했음!", Toast.LENGTH_LONG).show();
-
             }
         });
         dialog.show();
